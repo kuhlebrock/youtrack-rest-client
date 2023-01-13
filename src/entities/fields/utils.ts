28bae083cd64cdda712c51ac4d9127d33bbe4f6d
Tobias Kuhlebrock <tobias.kuhlebrock@qplix.com>
2023-01-13 20:44:03 +0100

    introduce max level

--- src/entities/fields/utils.ts
+++ src/entities/fields/utils.ts
@@ -6,14 +6,14 @@ const nestedField = (property: string, fields: string[]): string => {
     return `${property}(${fields.join(',')})`;
 };
 
-export const generateFields = (obj: GenericObject): string[] => {
+export const generateFields = (obj: GenericObject, maxLevel: number = 1, level:number = 0): string[] => {
     return Object.getOwnPropertyNames(obj).map(f => {
         const property = (<any>obj)[f];
-        if (typeof property === 'object' && property) {
+        if (typeof property === 'object' && property && level <= maxLevel) {
             if (!Array.isArray(property)) {
-                return nestedField(f, generateFields(property));
+                return nestedField(f, generateFields(property, maxLevel, level+1));
             } else if (property.length > 0) {
-                return nestedField(f, generateFields(property[0]));
+                return nestedField(f, generateFields(property[0], maxLevel, level+1));
             }
         }
         return f;
